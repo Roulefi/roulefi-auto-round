8bce5b5abb93f1d8161bfd1e6c27ebee7944791b
bhc8521 <bhc8521>
2022-03-22 21:20:01 +0800

    test

--- spin.js
+++ spin.js
@@ -2,14 +2,22 @@ import wampApi from "./wamp/api.js";
 import contract from "./contract.js"
 
 let roundStatus = {}
+let spinning = false
 
 const handler = async (value) => {
   console.log(value.latestBlockHeight)
+  if (spinning) {
+    return
+  }
   try {
-    let contractStatus = await contract.get_contract_status()
-    if (roundStatus.next_round_block_index < Number(value.latestBlockHeight) && contractStatus.bet_count > 0) {
-      await contract.spin_wheel()
-      roundStatus = await contract.get_round_status()
+    if (roundStatus.next_round_block_index < Number(value.latestBlockHeight)) {
+      let contractStatus = await contract.get_contract_status()
+      if (contractStatus.bet_count > 0) {
+        spinning = true
+        await contract.spin_wheel()
+        spinning = false
+        roundStatus = await contract.get_round_status()
+      }
     }
   } catch (e) {
     console.log(e)
